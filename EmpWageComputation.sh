585326587201b1d360249b9d43e7be1186bbbb8b
ShreyasNandish <shreyasnandish4@gmail.com>
2022-06-24 17:06:40 +0530

    [Shreyas] Employee Attendance

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,46 +1,11 @@
 #!/bin/bash -x
 
-TotalWorkingHrs() {
-while [[ $totalEmpHrs -ne $MaxWorkinghourinmonth && $totalWorkingDays -ne $Monthworkdays ]]
-do
-        ((totalWorkingDays++))
-        random=$(( $RANDOM % 3 ))
+echo "Attendence"
+random=$(($RANDOM % 2))
 
-
-        case $random in
-        1)
-                #Present fullday
-                empHrs=8
-                ;;
-        2)
-                #Present halfday
-                empHrs=4
-                ;;
-        *)
-                #absent
-                empHrs=0
-                ;;
-        esac
-
-        totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
-done
-}
-
-Monthworkdays=20
-MaxWorkinghourinmonth=100
-WagePerHour=20
-FullDayHour=8
-HalfDayHour=4
-monthly_Wage=0
-
-totalEmpHrs=0
-totalWorkingDays=0
-
-echo "Calculate work hours for one month"
-
-TotalWorkingHrs
-
-echo "Total working hours for a month $totalEmpHrs"
-
-
-
+if [ $random -eq 1 ]
+then
+	echo "Present"
+else
+	echo "Absent"
+fi
