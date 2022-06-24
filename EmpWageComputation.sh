0fbffa4596dfe13835bd387632cf6f6c3725c71c
ShreyasNandish <shreyasnandish4@gmail.com>
2022-06-24 13:07:25 +0530

    [shreyas] Calculation wage of daily and monthly

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -7,6 +7,7 @@ ispresent=1;
 empRateperhour=20;
 empfulldayhour=8;
 emphalfdayhour=4;
+Monthworkdays=20;
 
 empcheck=$((RANDOM%3))
 
@@ -14,11 +15,15 @@ case $empcheck in
 	0)
 		 echo "Employee is working full time"
 	         DailyWage=$(($empRateperhour * $empfulldayhour))
-        	 echo "Emploee daily wage is" $DailyWage;;
+		 MonthlyWage=$(($DailyWage*$Monthworkdays))
+        	 echo "Emploee daily wage is" $DailyWage
+		 echo "Employee monthly wage is" $MonthlyWage;;
 	1)
                  echo "Employee is working part time"
                  DailyWage=$(($empRateperhour * $emphalfdayhour))
-                 echo "Emploee daily wage is" $DailyWage;;
+		 MonthlyWage=$(($DailyWage*$Monthworkdays))
+	         echo "Emploee daily wage is" $DailyWage
+		 echo "Employee monthly wage is" $MonthlyWage;;
 	2)
 		echo "Employee is absent";;
 esac
