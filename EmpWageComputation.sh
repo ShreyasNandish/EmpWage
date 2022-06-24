8965ae1cc2e9ddd66b83a70d62dc9e3fa93b1635
ShreyasNandish <shreyasnandish4@gmail.com>
2022-06-24 12:42:01 +0530

    [Shreyas] Calculating wage as per full or half day

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,3 +1,30 @@
 #! /bin/bash -x
 
 echo " Hello welcome to Employee wage program"
+
+ispresent=1;
+empRateperhour=20;
+empfulldayhour=8;
+emphalfdayhour=4;
+
+empcheck=$((RANDOM%2))
+DailyWage=0;
+
+a=$((RANDOM%3))
+
+if [ $a -eq 0 ]
+then
+        echo "Employee is working full time"
+	DailyWage=$(($empRateperhour * $empfulldayhour))
+        echo "Emploee daily wage is" $DailyWage
+
+elif [ $a -eq 1 ]
+then
+        echo "Employee is working part time"
+        DailyWage=$(($empRatePerHour * $emphalfdayhour))
+        echo "Emploee daily wage is" $DailyWage
+
+else
+	echo "Employee is absent"
+fi
+
