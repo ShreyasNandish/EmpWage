d6d111a1bc25550ad3429ee8b2ccee39f2a9064c
ShreyasNandish <shreyasnandish4@gmail.com>
2022-06-24 12:11:35 +0530

    [Shreyas] Employee wage calculation

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,3 +1,18 @@
 #! /bin/bash -x
 
 echo " Hello welcome to Employee wage program"
+
+ispresent=1;
+empRateperhour=20;
+empfulldayhour=8;
+empcheck=$((RANDOM%2))
+
+if(($ispresent==$empcheck))
+then
+        echo "Employee is Present"
+        salary=$(($empRateperhour*$empfulldayhour))
+        echo $salary
+else
+        echo "Employee is absent"
+fi
+
